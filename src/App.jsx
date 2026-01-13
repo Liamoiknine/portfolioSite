d0927e9f6dae743d9349841c71a23972bc737418
Liamoiknine <l.j.oiknine@wustl.edu>
2026-01-13 17:44:54 +1100

    remove video

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Portfolio from './components/Portfolio.jsx'
-import ThriveVideo from './components/ThriveVideo.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Portfolio />} />
-      <Route path="/thrive-video" element={<ThriveVideo />} />
     </Routes>
   )
 }
